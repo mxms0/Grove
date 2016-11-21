726f067950b527b7437348ca64e69cafe5c9c739
Rocco Del Priore <roccodelpriore@me.com>
2016-11-20 21:36:53 -0700

    Set up structure for branches & commits.

---
+++ Grove/GRCommitsModel.h
@@ -0,0 +1,13 @@
+//
+//  GRCommitsModel.h
+//  Grove
+//
+//  Created by Rocco Del Priore on 11/13/16.
+//  Copyright © 2016 Milo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GRCommitsModel : NSObject
+
+@end
---
+++ Grove/GRCommitsModel.m
@@ -0,0 +1,13 @@
+//
+//  GRCommitsModel.m
+//  Grove
+//
+//  Created by Rocco Del Priore on 11/13/16.
+//  Copyright © 2016 Milo. All rights reserved.
+//
+
+#import "GRCommitsModel.h"
+
+@implementation GRCommitsModel
+
+@end
